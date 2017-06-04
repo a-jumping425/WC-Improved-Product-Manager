8c6998ffa4e395130d2640e0b824c2fa9469cfeb
Jumping <a.jumping425@gmail.com>
2017-06-04 12:55:42 +0700

    Add WCImprovedProductManager class

--- index.php
+++ index.php
@@ -8,3 +8,56 @@
  * License: GPL
  */
 
+if ( ! class_exists( 'WCImprovedProductManager' ) ) :
+
+    /**
+     * "WooCommerce Improved Product Manager" Main Class
+     *
+     * @class WCImprovedProductManager
+     * @version	1.0
+     */
+    final class WCImprovedProductManager {
+        protected static $_instance = null;
+
+        /**
+         * Instance
+         *
+         * Ensures only one instance of WCImprovedProductManager is loaded or can be loaded.
+         *
+         * @since 1.0
+         * @static
+         * @return WCImprovedProductManager - instance
+         */
+        public static function instance() {
+            if ( is_null( self::$_instance ) ) {
+                self::$_instance = new self();
+            }
+            return self::$_instance;
+        }
+
+        /**
+         * Constructor.
+         * @access public
+         * @return WCImprovedProductManager
+         */
+        public function __construct() {
+            // Plugin Activation
+            register_activation_hook( __FILE__, array($this, 'activate') );
+
+            // Plugin Deactivation
+            register_deactivation_hook( __FILE__, array($this, 'deactivate') );
+        }
+
+        public function activate() {
+
+        }
+
+        public function deactivate() {
+
+        }
+    }
+
+endif;
+
+// Global for backwards compatibility.
+$GLOBALS['wc_eps'] = WCImprovedProductManager::instance();
