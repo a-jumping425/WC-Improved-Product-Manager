66fdde422b4c464a9d4b7122387ce6671477b205
Jumping <a.jumping425@gmail.com>
2017-06-04 12:54:59 +0700

    Add plugin info.

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: WooCommerce - Improved Product Manager
+ * Plugin URI: https://wordpress.org/plugins/
+ * Description: Enhance WooCommerce product search and view.
+ * Version: 1.0
+ * Author: Jumping
+ * License: GPL
+ */
+
